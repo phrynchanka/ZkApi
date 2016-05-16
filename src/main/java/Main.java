75d133a7a4f9cf7d48da4b85f8ee159615516817
phrynchanka <pashagrinchenko@mail.ru>
2016-05-16 19:38:14 +0300

    changed

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Pavel Hrynchanka on 01.03.2016.
- */
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/util/ValidationHost.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-/**
- * Created by Pavel Hrynchanka on 02.03.2016.
- */
-public class ValidationHost {
-}
